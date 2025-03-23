85437ffabccbdf0bcff87d01ff439ea62457a719
Rawann0m <raawannm@gmail.com>
2025-03-23 13:05:26 +0300

    Initial Commit

---
+++ ClassTaskW4D1/ClassTaskW4D1App.swift
@@ -0,0 +1,17 @@
+//
+//  ClassTaskW4D1App.swift
+//  ClassTaskW4D1
+//
+//  Created by Rawan on 23/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ClassTaskW4D1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassTaskW4D1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClassTaskW4D1
+//
+//  Created by Rawan on 23/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
